21c3129e4cafd89f8c28440a581406bf12fdbdb1
Vani918 <usharanipoojary121@gmail.com>
2026-03-10 21:21:05 +0530

    Remove logger middleware implementation

--- Day_9/middleware/logger.js
+++ Day_9/middleware/logger.js
@@ -1,12 +1,1 @@
-const loggerMiddleware = (req, res, next) => {
 
-    const requestTime = new Date().toISOString();
-    const method = req.method;
-    const url = req.originalUrl;
-
-    console.log(`Time: ${requestTime} | Method: ${method} | URL: ${url}`);
-
-    next();
-};
-
-module.exports = loggerMiddleware;
