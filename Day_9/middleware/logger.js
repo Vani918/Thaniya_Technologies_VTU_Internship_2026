62218d6f6cbe088cbb8518a1fe6c6f925465794c
Vani918 <usharanipoojary121@gmail.com>
2026-03-10 21:16:29 +0530

    Add logger middleware for request logging

--- Day_9/middleware/logger.js
+++ Day_9/middleware/logger.js
@@ -1,1 +1,12 @@
+const loggerMiddleware = (req, res, next) => {
 
+    const requestTime = new Date().toISOString();
+    const method = req.method;
+    const url = req.originalUrl;
+
+    console.log(`Time: ${requestTime} | Method: ${method} | URL: ${url}`);
+
+    next();
+};
+
+module.exports = loggerMiddleware;
