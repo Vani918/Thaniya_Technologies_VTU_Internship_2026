3c50c13a8c9e50ab90a5b79a310fdd5c1857b125
Vani918 <usharanipoojary121@gmail.com>
2026-03-11 21:04:27 +0530

    Create students.js

---
+++ Day_10/Project3/data/students.js
@@ -0,0 +1,14 @@
+let students = [
+    {
+        id: 1,
+        name: "Rahul",
+        course: "Computer Science"
+    },
+    {
+        id: 2,
+        name: "Priya",
+        course: "Information Technology"
+    }
+];
+
+module.exports = students;
