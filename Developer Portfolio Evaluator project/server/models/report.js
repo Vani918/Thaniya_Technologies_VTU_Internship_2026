0bd8b73f5d9ecad68703d3fdaf14c8c78e22d2e5
Vani918 <usharanipoojary121@gmail.com>
2026-03-26 22:58:09 +0530

    added MongoDB schema for storing evaluation reports

---
+++ Developer Portfolio Evaluator project/server/models/report.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const reportSchema = new mongoose.Schema({
+  username: String,
+  score: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Report", reportSchema);
