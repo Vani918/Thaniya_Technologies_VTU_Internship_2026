e74318aac9baa151e4a3138633707cd041485555
Vani918 <usharanipoojary121@gmail.com>
2026-04-01 21:32:11 +0530

    Refactor scoring.js for consistent formatting

--- Developer Portfolio Evaluator project/server/utils/scoring.js
+++ Developer Portfolio Evaluator project/server/utils/scoring.js
@@ -1,23 +1,23 @@
 function calculateScore(user, repos) {
-  let score = 0;
+let score = 0;
 
-  // Activity
-  if (repos.length > 10) score += 20;
-  if (repos.length > 20) score += 10;
+// Activity
+if (repos.length > 10) score += 20;
+if (repos.length > 20) score += 10;
 
-  // Stars
-  let totalStars = repos.reduce((acc, repo) => acc + repo.stargazers_count, 0);
-  if (totalStars > 50) score += 20;
+// Stars
+let totalStars = repos.reduce((acc, repo) => acc + repo.stargazers_count, 0);
+if (totalStars > 50) score += 20;
 
-  // Diversity
-  let languages = new Set(repos.map(r => r.language));
-  if (languages.size > 3) score += 20;
+// Diversity
+let languages = new Set(repos.map(r => r.language));
+if (languages.size > 3) score += 20;
 
-  // Profile completeness
-  if (user.bio) score += 10;
-  if (user.avatar_url) score += 10;
+// Profile completeness
+if (user.bio) score += 10;
+if (user.avatar_url) score += 10;
 
-  return score;
+return score;
 }
 
 module.exports = { calculateScore };
