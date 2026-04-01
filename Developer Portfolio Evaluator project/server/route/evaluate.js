40237b7f9aad913ba28f6eaaf2b6ac297a3f6b1e
Vani918 <usharanipoojary121@gmail.com>
2026-04-01 21:30:36 +0530

    Add route-level logging to debug API request flow

--- Developer Portfolio Evaluator project/server/route/evaluate.js
+++ Developer Portfolio Evaluator project/server/route/evaluate.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { evaluateProfile } = require("../controllers/evaluateController");
 
-router.post("/evaluate", evaluateProfile);
+router.post("/evaluate", (req, res, next) => {
+  console.log("Route hit!");
+  next();
+}, evaluateProfile);
+
 
 module.exports = router;
