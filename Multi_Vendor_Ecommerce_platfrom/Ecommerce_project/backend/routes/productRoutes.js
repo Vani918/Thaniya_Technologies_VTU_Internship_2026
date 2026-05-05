b91a0976de1019dcecceaefa952f6b19fce00e13
Vani918 <usharanipoojary121@gmail.com>
2026-05-05 20:40:52 +0530

    Add product routes for creating and retrieving products

---
+++ Multi_Vendor_Ecommerce_platfrom/Ecommerce_project/backend/routes/productRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const Product = require("../models/Product");
+
+// Add product (vendor)
+router.post("/", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.json(product);
+});
+
+// Get all products
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+module.exports = router;
+
